7148f3e7bf8c474ea5672b071b3b602a37b6a41b
mars-sudo <robert.martin92@protonmail.com>
2023-05-12 15:03:13 -1000

    Added Python code references.

--- py-edu/4-Dictionaries.py
+++ py-edu/4-Dictionaries.py
@@ -1,67 +1,167 @@
-# Variables
-print("Hello World")
-name = "mars"
-print(f"Hello {name.title()}")
-math = 2 + 5
-print(f"{math}\t\n")
+# A Dictionary is a collection of key-value pairs.
+# Each key is connected to a value and you can use a key to access the value associated with that key.
+# A key-value is a set of values associated with each other. 
 
-# Lists
-feelings =['peace','joy','clarity','optimisim']
-print(feelings)
-print(feelings[0])
-print(f"Today I am feeling lots of {feelings[0].upper()}!")
+house_0 = {'location':'seattle', 'size':'900 sqft','bedrooms':'2'}
+print(house_0)
+print(house_0['location'])
+print(house_0['size'])
+del house_0['bedrooms'] # Be aware that the deleted key-value pair is removed permanently. 
+print(house_0)
 
-feelings.append('grief')
-print(feelings)
-del feelings[1]
-print(feelings)
+new_house = house_0['size']
+print(f"The new house size is {new_house}.\n")
 
-# For Loops
-empty_list = []
-for value in range(1,5):
-    multiple_of_two = value*2
-    empty_list.append(multiple_of_two)
-print(empty_list)
+house_0['amenities'] = 'pool'
+house_0['neigborhood']='downtown'
+print(house_0)
 
-empty_list_2 = []
-for value_2 in range(1,5):
-    empty_list_2.append(value_2*2)
-print(empty_list_2)
+house_1 ={}
+house_1['location']='austin'
+house_1['size']='2,000 sqft'
+print(f"the current location of this house 1 is in {house_1['location'].title()}\n")
 
-empty_list_3 = [value_3*2 for value_3 in range(1,5)]
-print(empty_list_3)
+house_1['location']='toronto'
+print(f"the new location of this house 1 is in {house_1['location'].title()}\n")
 
-# Slicing
-print(feelings)
-print(feelings[0:2])
-print(feelings[0:3])
-print(feelings[1::2])
+print(f"Original position of house 0 is : {house_0['location'].title()}.\n")
 
-# Tuples
-# Tuples are lists that are immutable, the value cannot change. 
-# A tuple uses parentheses instead of square brackets
-dimensions = (200, 50)
-print(dimensions[0])
-print(dimensions[1])
+#########################################################################################################
 
-# Looping over Tuples
-for dimension in dimensions:
-    print(dimension)
+# You can also use a dictionary to store one kind of information about many objects. 
+# For example, say you want to poll a number of people and ask them what their favorite programming language is. 
+# A dictionary is useful for storing the resultes of a simple poll.
+favorite_languages ={
+    'lucy':'c',
+    'reina':'go',
+    'keisha':'php',
+    'david':'python'
+}
 
-# Pg 68
-foods = ('ramen','korean-bbq','poke','thai','local')
-for food in foods:
-    print(food)
+language = favorite_languages['keisha'].title()
+print(f"Keisha's favorite language is {language}.\n")
+print(f"{favorite_languages['reina'].title()}\n")
 
-#foods[0]=('sushi')
-#print (food)
+# create names for the two variables tht will hold the key and value in each key-value pair.
+# you can choose any names you want for these two variables. 
 
-foods = ('chinese','fast-food','ramen','korean-bbq','poke','thai','hawaiian')
-for food in foods:
-    print (f"\n{food}")
+# SYNTAX - for k, v in dictionary.items():
+for name, language in favorite_languages.items():
+    print(f"\n{name.title()}'s favorite language is {language.title()}.")
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+friends = ['reina','david']
+for name in favorite_languages.keys():
+    print(name.title())
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+if 'claude' not in favorite_languages.keys():
+    print(f"Claude, please take the poll.")
+
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, is now in a sorted list.")
+
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.lower())
+
+for language in set(favorite_languages.values()):  
+    print(language.upper())
+# When you wrap set() around a list that contains duplicate items, Python identifies the unique items in the list
+# and builds a set from those items. 
 
 
+# For dictionaries you can use get() method to set a default value that will be retured if the requested key doesn't exist
+alien_1 = {'color': 'green','speed':'slow'}
+point_value = alien_1.get('points','No point value assigned.')
+print(f"\n{point_value}\n")
+
+# You can loop through all of a dictionary's key-value pairs, through its keys, or through its values. 
+user_0 ={
+    'username':'mkalapana',
+    'first':'mike',
+    'last':'kalapana'
+}
+
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"\nValue: {value}")
 
 
+###########################################################################
+# Modify values in Dictionary
+alien_0 ={'x_position':0, 'y-position':25,'speed':'medium'}
+print(f"Original position: {alien_0['x_position']}.\n")
 
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed. 
+if alien_0['speed'] =='slow':
+    x_increment = 1
+elif alien_0['speed']=='medium':
+    x_increment = 2
+else:
+    x_increment = 3
 
+# The new position is the old position plus the increment
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(f"The new position is: {alien_0['x_position']}.\n")
+################################################################################################
+
+# Nesting 
+
+## A List of Dictionaries
+
+npc_0 = {'color':'red','points':'3'}
+npc_1 = {'color':'green','points':'2'}
+npc_2 = {'color':'blue','points':'1'}
+
+npcs =[npc_0,npc_1,npc_2]
+
+for npc in npcs:
+    print(npc)
+
+# Make an empty list for storing npc's
+npcs = []
+
+# Make 30 red npc's
+for npc_number in range(30):
+    new_npc = {'color':'red','points':'5'}
+    npcs.append(new_npc)
+
+# Show the first 5 npc's
+for npc in npcs[:5]:
+    print(npc)
+
+print("...")
+
+for npc in npcs[:3]:
+    if npc['color'] =='red':
+        npc['color'] = 'orange'
+        npc['points'] = '8'
+    print(npc)
+
+# A List in a Dictionary
+pizza = {
+    'crust':'thick',
+    'toppings' : ['mushrooms','olives'],
+}
+
+print(f"You ordered a {pizza['crust']} -crust pizza with the following toppings")
+for topping in pizza['toppings']:
+    print("\t" + topping)
+
+examples = {
+    'person_1':['english','spanish'],
+    'person_2':['french','swedish'],
+    'person_3':['hindi','mandarin'],
+}
+
+for name, languages in examples.items():
+    print(f"\n{name.title()}'s languages are:")
+    for language in languages:
+        print(f"\t{language.upper()}")
--- py-edu/5-User-Input_While-Loops.py
+++ py-edu/5-User-Input_While-Loops.py
@@ -1,30 +1,124 @@
-cars=['audi','bmw','subaru','toyota']
-for car in cars:
-    if car =="bmw":  
-        print(car.upper())
+# Use the input() functions to accept user input
+# the input() function pauses your program and waits for the user to enter some text. 
+
+name = input("Please enter your name: ")
+print(f"\nHello, {name}")
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name?" 
+# the operator += takes the string that was assigned to prompt and adds the new string onto the end
+name = input(prompt)
+print(f"\nHello, {name}!")
+
+age = input("How old are you? ")
+age =int(age)
+if age > 18:
+    print("You're older than 18!")
+
+number = input("Enter a number, and a I'll you if it's even or odd: ")
+number = int(number)
+
+if number % 2 == 0: # Modulo just tells you what the remainder is.
+    print(f"\nThe number {number} is even.") 
+# Even numbers are always divisible by 2, if the modulo of a number is two or zero then the number is even.
+else:
+    print(f"\nThe number {number} is odd.")
+
+#############################################################################################################
+# While loops
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1 # shorthand for current_number + 1
+
+##########################################################################################
+
+
+########################################################################################
+# Using Break to Exit a loop
+# To exit a while loop immediatley without running any remaining code in the loop, regardless of the results of any conditional test,
+# use the break statement. 
+# The break statement directs the flow of your program; you can use it to control which lines of code are executed and which
+# aren't, so the program only executes code that you want it to, when you want it to. 
+
+prompt_3 = "\nPlease enter the name of a city you have visited:"
+prompt_3 += "\n(Enter 'quit' when you are finished.)"
+
+while True: # A loop that starts with While True will run forever unless it reaches a break statement. 
+    city = input(prompt_3)
+
+    if city == 'quit':
+        break
     else:
-        print(car.title())
+        print(f"I'd love to go to {city.title()}!")
 
-# This can also be called a conditional test, for example if a username is unique as both a upper and lowercase. 
-# Equality Operator - ==
-# Inequality Operator - !=
+######################################################################################################
+# Avoid infite loops
+# this loop runs forever!
+# x = 1
+# while x <= 5:
+#   print(x)
+# Now the value of x will start at 1 but never change. 
+# As a result, the conditional test x <= 5 will always evaluate to True and the while loop will run forever printing 1's
 
-requested_topping ='mushrooms'
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
 
-age = 23
-age < 25 # The age is less than 25 --> True
-age <= 25 # The age is less than or equal too 25 --> True
-age > 25 # The age is greater than 25 --> False
-age >= 25 # The age is greater than or equal to 25 --> FALSE
+# this loop counts to 5
+x = 1
+while x<=5:
+    print(x)
+    x +=1 # This loop is counting from 1 to 5
 
-temp_0 = 70
-temp_1 = 90
-if (temp_0 >= 50) and (temp_1 >= 90):
-    print("Both temperatures are greater than 50.")
 
-temp_2 = 75
-temp_3 = 88
-if (temp_2 <= 70) or (temp_3 <= 88):
-    print("One or both temperatures are greater than 50.")
+########################################################################################################
+# Moving items from one list to another
+# Consider a list of newly registered but unverified users of a website. 
+# After we verify these users, how can we move them toa separate list of confirmed users?
+
+# Start with users that need to be verified, and an empty list ot hold confirmed users. 
+unconfirmed_users = ['alice','brian','candace']
+confirmed_users = []
+
+# Verify each user until there are no more confirmed users.
+# Move each verified user into the list of confirmed users.
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+
+    print(f"Verifying user: {current_user.title()}")
+    confirmed_users.append(current_user)
+
+# Display all confirmed users.
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
+
+
+##################################################################################
+responses ={}
+
+# Set a flag to indicate that polling is active.
+polling_active = True
+
+while polling_active:
+    # Prompt for the person's name and response
+    name = input("\nWhat is your name? ")
+    response = input("Which emotion do you feel most frequently? ")
+
+    # Store the response in the dictionary
+    responses[name] = response
+
+    # Find out if anyone else is going to take the poll.
+    repeat = input("Would you like to let another person respond? (yes/no)")
+    if repeat == 'no':
+        polling_active = False
+
+# Polling is complete. Show the results
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(f"{name} would like to clime {response}.")
+
+
+
+
+
+
+
