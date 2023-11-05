ccf0ead64d058008b080be7889ca654c61904a11
mars-sudo <robert.martin92@protonmail.com>
2023-11-05 18:37:06 -0500

    added security practices to project

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model): #"Topic" is the name of this model
     """A topic the user is learning about."""
     text = models.CharField(max_length=200) # Attribute associated with this Model
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def _str_(self):
         """Return a string representation of the model."""
