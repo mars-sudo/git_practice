a5a6c719fa48c7507052cfb5ee7aa4f24951c930
mars-sudo <robert.martin92@protonmail.com>
2023-11-07 15:16:28 -0500

    changes to settings.py

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,7 +139,7 @@ LOGIN_URL = 'users:login'
 import django_heroku
 django_heroku.settings(locals())
 
-# if os.environ.get('DEBUG')== 'TRUE':
+#if os.environ.get('DEBUG') == 'TRUE':
 #    DEBUG = True
-# elif os.environ.get('DEBUG') == 'FALSE':
+#elif os.environ.get('DEBUG') == 'FALSE':
 #    DEBUG = False
